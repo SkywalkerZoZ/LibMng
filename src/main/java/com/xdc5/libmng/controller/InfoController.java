6ecb91e410d09ccfb75acbffdacebba69e2eae0d
Wenhui Wei <2671176083@qq.com>
2024-03-27 22:47:34 +0800

    Completed the book information catalog and book information retrieval

--- src/main/java/com/xdc5/libmng/controller/InfoController.java
+++ src/main/java/com/xdc5/libmng/controller/InfoController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 
 @Slf4j
@@ -19,10 +21,11 @@ public class InfoController {
     @Autowired
     private BookService bookService;
     //
-    @GetMapping("/user/books/Info")
-    public Result showBookCatasInfo(){
-        List<BookInfo> bookInfoList=bookService.getAllBookInfos();
-        return Result.success(bookInfoList,"Success: books Info");
+    @GetMapping("/user/books/info")
+    public Result showBookInfo(){
+        //按照api要求返回信息。
+        List<HashMap<String,Object>> bookInfoList=bookService.getAllBookInfos();
+        return Result.success(bookInfoList,"Success: all books Info");
     }
 
     //
@@ -30,15 +33,19 @@ public class InfoController {
     public Result searchBook(@RequestBody Map<String, Object> requestBody){
         String method=(String)requestBody.get("method");
         String keyword=(String)requestBody.get("keyword");
-        if(method=="title"){
+        log.info("method:"+method+"keyword:"+keyword);
+        if(Objects.equals(method, "title")){
             //按照title寻找数目并返回success
-            return Result.success("hh");
-        }else if(method=="author"){
+            List<HashMap<String,Object>> allBooksInfo=bookService.getBookByTitle(keyword);
+            return Result.success(allBooksInfo,"Success: books Info");
+        }else if(Objects.equals(method, "author")){
             //按照author寻找书目并返回success
-            return Result.success("hh");
-        }else if(method=="isbn"){
+            List<HashMap<String,Object>> allBooksInfo=bookService.getBookByAuthor(keyword);
+            return Result.success(allBooksInfo,"Success: books Info");
+        }else if(Objects.equals(method, "isbn")){
             //按照isbn寻找书目并返回success
-            return Result.success("hh");
+            List<HashMap<String,Object>> allBooksInfo=bookService.getBookByIsbn(keyword);
+            return Result.success(allBooksInfo,"Success: books Info");
         }else{
             return Result.error("Fail: invalid method");
         }
--- src/main/java/com/xdc5/libmng/mapper/BookInfoMapper.java
+++ src/main/java/com/xdc5/libmng/mapper/BookInfoMapper.java
@@ -1,10 +1,13 @@
 package com.xdc5.libmng.mapper;
 
 import com.xdc5.libmng.entity.BookInfo;
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface BookInfoMapper {
@@ -15,6 +18,10 @@ public interface BookInfoMapper {
     int addBookInfo(BookInfo bookInfo);
     int updateBookInfo(BookInfo bookInfo);
     List<BookInfo> getBookInfoByISBN(@Param("isbn") String isbn);
-
-
+//按照api要求返回查找信息
+    List<HashMap<String,Object>> getBookByTitle(String title);
+    List<HashMap<String,Object>> getBookByAuthor(String author);
+    List<HashMap<String,Object>> getBookByIsbn(String isbn);
+//按照api要求返回图书信息。
+    List<HashMap<String,Object>> getAllBookInfo();
 }
