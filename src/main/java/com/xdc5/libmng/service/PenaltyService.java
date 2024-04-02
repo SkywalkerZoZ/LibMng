ad4b5c18d2dbbd2424ad9c192405443047a3c860
Skywalker <herobrinex@163.com>
2024-04-02 20:28:44 +0800

    fix penalty

--- src/main/java/com/xdc5/libmng/service/PenaltyService.java
+++ src/main/java/com/xdc5/libmng/service/PenaltyService.java
@@ -1,10 +1,13 @@
 package com.xdc5.libmng.service;
 
 
+import com.xdc5.libmng.entity.Penalty;
 import com.xdc5.libmng.mapper.PenaltyMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Slf4j
 @Service
@@ -13,10 +16,20 @@ public class PenaltyService {
     private PenaltyMapper penaltyMapper;
     //插入新的处分信息
     public void insertNewPenalty(Integer adminId,Integer userId,String reason,String endDate){
-        penaltyMapper.insertNewPenalty(adminId,userId,reason,endDate);
+        Penalty penalty=new Penalty();
+        penalty.setAdminId(adminId);
+        penalty.setUserId(userId);
+        penalty.setReason(reason);
+        penalty.setEndDate(LocalDate.parse(endDate));
+        penaltyMapper.addPenalty(penalty);
     }
     //将adminid和useid确定下的Penalty的reason和endDate信息进行更新，同时要在动态SQl中更新处分时间。
     public void updatePenaltyById(Integer adminId,Integer userId,String reason,String endDate){
-        penaltyMapper.updatePenaltyById(adminId,userId,reason,endDate);
+        Penalty penalty=new Penalty();
+        penalty.setAdminId(adminId);
+        penalty.setUserId(userId);
+        penalty.setReason(reason);
+        penalty.setEndDate(LocalDate.parse(endDate));
+        penaltyMapper.updatePenalty(penalty);
     }
 }
